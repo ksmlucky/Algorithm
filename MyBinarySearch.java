2deabecf2072c88eb65ddb313a702b80a5159744
ksmlucky <77111523+ksmlucky@users.noreply.github.com>
2021-09-17 22:45:55 +0900

    이진 탐색 구현

---
+++ MyBinarySearch.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class MyBinarySearch {
+    public boolean searchFunc(ArrayList<Integer> dataList, Integer searchItem) {
+        if (dataList.size() == 1 && searchItem == dataList.get(0)) {
+            return true;
+        }
+        if (dataList.size() == 1 && searchItem != dataList.get(0)) {
+            return false;
+        }
+        if (dataList.size() == 0) {
+            return false;
+        }
+
+        int medium = dataList.size() / 2;
+
+        if (searchItem == dataList.get(medium)) {
+            return true;
+        } else {
+            if (searchItem < dataList.get(medium)) {
+                return this.searchFunc(new ArrayList<Integer>(dataList.subList(0, medium)), searchItem);
+            } else {
+                return this.searchFunc(new ArrayList<Integer>(dataList.subList(medium, dataList.size())), searchItem);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        MyBinarySearch binarySearch = new MyBinarySearch();
+
+        ArrayList<Integer> testData = new ArrayList<Integer>();
+
+        for (int i = 0; i < 100; i++) {
+            testData.add((int) (Math.random() * 100));
+        }
+        Collections.sort(testData);
+
+        System.out.println(testData);
+
+        System.out.println(binarySearch.searchFunc(testData, 99));
+    }
+}
